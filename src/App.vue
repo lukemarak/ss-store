247f14a4437b6feec1fee870ce1a3d6e93a91697
lukemarak <lukemarak@gmail.com>
2019-05-22 12:28:07 +0530

    modified

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ title }}</h1>
+    <ul v-for="item in appointments">
+	    <li>{{ item.petName }}</li>
+	    <li>{{ item.petOwner }}</li>
+	</ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+var apts = [{"petName":"Spot","petOwner":"Constance Smith",
+		"aptDate":"2017-07-24 08:30",
+		"aptNotes":"This German Shepherd is having some back pain"},
+		{"petName":"Goldie","petOwner":"Barot Bellingham",
+		"aptDate":"2017-07-22 15:50",
+		"aptNotes":"This Goldfish has some weird spots in the belly"},
+		{"petName":"Mitten","petOwner":"Hillary Goldwyn",
+		"aptDate":"2017-07-21 9:15",
+		"aptNotes":"Cat has excessive hairballs"},
+		{"petName":"Buffy","petOwner":"Hassum Harrod","aptDate":"2017-07-20 15:30",
+		"aptNotes":"This Chihuahua has not eaten for three days and is lethargic"}];
+		
+		
+		
+		
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	name: 'MainApp',
+	data() {
+		return {
+			title : 'Appointments',
+			appointments : apts
+		}
+	}
 }
 </script>
 
